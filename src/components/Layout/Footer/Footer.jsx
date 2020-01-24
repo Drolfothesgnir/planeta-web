328caa8aa01996376992dbc9ebf4a951c5dc4d13
drolfothesgnit <kyryl.yeletsky@gmail.com>
2020-01-25 00:24:55 +0200

    added new styles for main page

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import classes from './Footer.module.less';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import classes from "./Footer.module.less";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
   return (
     <footer className={classes.footer}>
-      <div  className='container'>
+      <div className="container">
         <div className={classes.footerContent}>
-          <a className="footer_phone" href="tel:+380 (93) 254 35 65">
-            <FontAwesomeIcon icon="phone-alt" />
-            <span>+380 (93) 254 35 65</span>
-          </a>
+          <div className={classes.link}>
+            <a className="footer_phone" href="tel:+380 (93) 254 35 65">
+              <FontAwesomeIcon icon="phone-alt" />
+              <span>+380 (93) 254 35 65</span>
+            </a>
+          </div>
+          <div className={classes.link}>
+            <a href="mailto:order@planeta-web.com.ua" className="footer_mail">
+              <FontAwesomeIcon icon="envelope" />
+              <span>order@planeta-web.com.ua</span>
+            </a>
+          </div>
+          <div className="copyright">
+            &copy; 2006-{new Date().getFullYear()}
+          </div>
         </div>
       </div>
     </footer>
