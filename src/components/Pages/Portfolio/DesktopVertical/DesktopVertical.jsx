72f23f21f4f90657b7e57360300f0a07644b1bf1
kyrylE <kyryl.yeletsky@gmail.com>
2020-02-26 17:40:21 +0200

    added new styles for portfolio page (vertical view);

--- src/components/Pages/Portfolio/DesktopVertical/DesktopVertical.jsx
+++ src/components/Pages/Portfolio/DesktopVertical/DesktopVertical.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import Slider from "../../../Utilities/Slider/Slider";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons/faCaretRight";
 import classes from "./DesktopVertical.module.less";
 
+function getPrevAndNextIndex(n, current) {
+  return current === 0
+    ? [n - 1, 1]
+    : current === n - 1
+    ? [n - 2, 0]
+    : [current - 1, current + 1];
+}
+
 function DesktopVertical({ items }) {
-  const [state, setState] = React.useState({
+  const sliderRef = React.useRef(null);
+  const [{ currentSlide }, setState] = React.useState({
     currentSlide: 0
   });
+  const [prevIndex, nextIndex] = getPrevAndNextIndex(
+    items.length,
+    currentSlide
+  );
+
+  const prev = () => sliderRef.current.slickPrev();
+  const next = () => sliderRef.current.slickNext();
+
   return (
     <div className={classes.desktopVertical}>
       <Slider
@@ -19,7 +38,8 @@ function DesktopVertical({ items }) {
           arrows: false,
           beforeChange(_, next) {
             setState(prev => ({ ...prev, currentSlide: next }));
-          }
+          },
+          ref: sliderRef
         }}
       >
         {items.map(({ title, buttonText, link, imgSrc }) => {
@@ -33,16 +53,35 @@ function DesktopVertical({ items }) {
                     {buttonText}
                   </Link>
                 </div>
-                <div className={classes.image}>
+                <Link to={link} className={classes.image}>
                   <img src={imgSrc} alt={title} />
-                </div>
+                </Link>
               </div>
             </div>
           );
         })}
       </Slider>
-      <h2 style={{ position: "absolute", top: "50%" }}>{`${state.currentSlide +
-        1}/${items.length}`}</h2>
+      <p className={classes.counter}>
+        <button onClick={prev}>
+          <FontAwesomeIcon icon={faCaretRight} />
+        </button>
+        <span>{`${currentSlide + 1}/${items.length}`}</span>
+        <button onClick={next}>
+          <FontAwesomeIcon icon={faCaretRight} />
+        </button>
+      </p>
+      <button
+        className={`${classes.verticalButton} ${classes.prev}`}
+        onClick={prev}
+      >
+        <span>{items[prevIndex].title}</span>
+      </button>
+      <button
+        className={`${classes.verticalButton} ${classes.next}`}
+        onClick={next}
+      >
+        <span>{items[nextIndex].title}</span>
+      </button>
     </div>
   );
 }
