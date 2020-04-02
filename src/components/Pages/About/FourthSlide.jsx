532c8798fb7fe90cb322bc77f56673ef1063788e
kerel <kyryl.yeletsky@gmail.com>
2020-04-02 19:03:26 +0300

    new build

--- src/components/Pages/About/FourthSlide.jsx
+++ src/components/Pages/About/FourthSlide.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./About.module.less";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ContactForm from '../../Utilities/ContactForm/ContactForm';
+import ContactForm from "../../Utilities/ContactForm/ContactForm";
 
 function FourthSlide({
   content: {
@@ -14,21 +14,28 @@ function FourthSlide({
       <div className={classes.content}>
         <div className={classes.left}>
           <span className={classes.mainTitle}>{mainTitle}</span>
-          <ContactForm className={classes.form} buttonClassname={'btn btn-dark'} />
+          <ContactForm
+            className={classes.form}
+            buttonClassname={"btn btn-dark"}
+          />
         </div>
         <div className={classes.right}>
           <span className={classes.title}>{title}</span>
           <ul className={classes.text}>
             {items.map(({ value, type }) => (
               <li key={value} className={classes[type]}>
-                <i><FontAwesomeIcon icon={type} /></i>
+                <i>
+                  <FontAwesomeIcon icon={type} />
+                </i>
                 <span>{value}</span>
               </li>
             ))}
           </ul>
           <span className={classes.title}>{followUs}</span>
           <div>
-            <i><FontAwesomeIcon icon={["fab", "facebook-f"]} /></i>
+            <i>
+              <FontAwesomeIcon icon={["fab", "facebook-f"]} />
+            </i>
             <span>planetawebcomua</span>
           </div>
         </div>
